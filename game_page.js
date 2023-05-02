db1ae6d839b9a3481467378ab4710c3420467438
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-05-02 15:12:56 -0300

    Update game_page.js

--- game_page.js
+++ game_page.js
@@ -6,14 +6,14 @@
 	player1Score = 0;
 	player2Score = 0;
 
-document.getElementById("player1Name").innerHTML = player1Name + " : ";
-document.getElementById("player2Name").innerHTML = player2Name + " : ";
+document.getElementById("player1Name").? = player1Name + " : ";
+document.getElementById("player2Name").? = player2Name + " : ";
 
-document.getElementById("player1Score").innerHTML = player1Score ;
-document.getElementById("player2Score").innerHTML = player2Score ;
+document.getElementById("player1Score").? = player1Score ;
+document.getElementById("player2Score").? = player2Score ;
 
-document.getElementById("playerQuestion").innerHTML = "Turno de Pergunta - " + player1Name ;
-document.getElementById("playerAnswer").innerHTML = "Turno de Resposta - " + player2Name ;
+document.getElementById("playerQuestion").? = "Turno de Pergunta - " + player1Name ;
+document.getElementById("playerAnswer").? = "Turno de Resposta - " + player2Name ;
 
 
 function send() {
@@ -22,20 +22,20 @@ function send() {
 	console.log("palavra em caixa baixa = " + word);
 
     charAt1 = word.charAt(1);
-	console.log(charAt1);
+	
 
 	lenghtDivide2 = Math.floor(word.length/2);
 	charAt2 = word.charAt(lenghtDivide2);
-	console.log(charAt2);
+	
 
     lenghtMinus1 = word.length - 1; 
     charAt3 = word.charAt(lenghtMinus1); 
-	console.log(charAt3);
+	
 
     removeCharAt1 = word.replace(charAt1, "_");
     removeCharAt2 = removeCharAt1.replace(charAt2, "_");
     removeCharAt3 = removeCharAt2.replace(charAt3, "_");
-	console.log(removeCharAt3);
+	
 
     question_word = "<h4 id='wordDisplay'> P. "+removeCharAt3+"</h4>";
     inputBox = "<br>Resposta : <input type='text' id='inputCheckBox'>";
